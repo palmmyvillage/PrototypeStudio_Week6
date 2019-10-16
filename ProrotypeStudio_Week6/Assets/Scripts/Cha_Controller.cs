1b0a4cde490409aeb636c2ca37da25a755eb5f74
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2019-10-16 02:18:13 -0400

    Save Midway

---
+++ ProrotypeStudio_Week6/Assets/Scripts/Cha_Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cha_Controller : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float H_speed;
+    public float jumpForce;
+
+    private bool touchGround;
+    private float V_H, V_V;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        V_H = Input.GetAxis("Horizontal") * H_speed;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+        
+        V_V = rb.velocity.y; 
+        rb.velocity = new Vector2(V_H,V_V);
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        
+    }
+}
